d5b9d1060e451bea8b74edcb89440875211adcbf
Al-kodach <alkodach351@gmail.com>
2023-04-09 15:23:26 -0400

    task-comlet

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,28 @@
-const Person = require('./person');
+const Person = require("./person");
 
-// Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(stud1, stud2) {
+    if (stud1.GPA > stud2.GPA) {
+      return `${stud1.firstName} ${stud1.lastName} has the higher GPA.`;
+    } else if (stud2.GPA > stud1.GPA) {
+      return `${stud2.firstName} ${stud2.lastName} has the higher GPA.`;
+    }
+
+    return `Both students have the same GPA`;
+  }
+}
+
+let student1 = new Student("billy", "johnson", "software-development", 3.2);
+let student2 = new Student("sarah", "smith", "basket-weaving", 3.7);
+
+console.log(Student.compareGPA(student1, student2));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
